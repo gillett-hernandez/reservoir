a36960124928d80ef8691deceebec730a2cb3dc7
Gillett Hernandez <gillett.hernandez@gmail.com>
2026-03-12 21:33:22 -0700

    rename method

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 pub struct Reservoir<T, const N: usize> {
-    data: [Option<T>; N],
-    count: usize,
+    pub data: [Option<T>; N],
+    pub count: usize,
 }
 impl<T, const N: usize> Default for Reservoir<T, N> {
     fn default() -> Self {
@@ -12,15 +12,15 @@ impl<T, const N: usize> Default for Reservoir<T, N> {
 }
 
 impl<T, const N: usize> Reservoir<T, N> {
-    pub fn record_with_random_value(&mut self, sample: T, uniform01_sample: f32) {
+    pub fn gather_with_given_random_value(&mut self, sample: T, uniform01_sample: f32) {
         if self.count < N || uniform01_sample < (self.count as f32).recip() {
             self.data[self.count % N] = Some(sample);
         }
         self.count += 1;
     }
     #[cfg(feature = "rand")]
-    pub fn record(&mut self, sample: T) {
-        self.record_with_random_value(sample, rand::random::<f32>());
+    pub fn gather(&mut self, sample: T) {
+        self.gather_with_given_random_value(sample, rand::random::<f32>());
     }
 
     pub fn iter_values(&self) -> impl Iterator<Item = &T> {
